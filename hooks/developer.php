a3dc9943d308c3ef67ab4088cbc46b8c1937744e
Michael S. Edwards <codingjungle@gmail.com>
2019-07-08 06:54:11 -0500

    test

--- hooks/developer.php
+++ hooks/developer.php
@@ -499,7 +499,7 @@ class toolbox_hook_developer extends _HOOK_CLASS_
                         if ( !\is_file( $hook ) ) {
                             $content = <<<EOF
 #!/bin/sh
-/usr/bin/php -r 'require "/home/michael/public_html/dev/init.php";\$gitHooks = (new \IPS\toolbox\GitHooks( ["{$app}"] ) )->removeSpecialHooks(true);'
+/usr/bin/php -r 'require "/home/michael/public_html/dev/init.php";\$gitHooks = (new \\IPS\\toolbox\\GitHooks( ["{$app}"] ) )->removeSpecialHooks(true);'
 EOF;
                             \file_put_contents( $hook, $content );
                         }
